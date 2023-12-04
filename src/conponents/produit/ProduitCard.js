d6119416fa86e8ea028c40273687b957df2e8a15
Changrui <m.zhang.changrui@gmail.com>
2023-12-04 17:05:21 +0100

    update

--- src/conponents/produit/ProduitCard.js
+++ src/conponents/produit/ProduitCard.js
@@ -5,33 +5,32 @@ import { authtificationService } from '../authentification/authentificationServi
 const ProduitCard = ({produit}) => {
     //obtenir img
     const [imageSrc, setImageSrc] = useState('');
-    const [path,setPath] = useState( (produit.imgs.length > 0)?produit.imgs[0].path:"vide");
-
+    const path =  (produit.imgs.length > 0)?produit.imgs[0].path:"";
     useEffect(() => {
-        axios.post("http://127.0.0.1:8080/api/produits/image", {
-            "path":path
-        },{
-            headers: {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*',
-                'Authorization': 'Basic ' + authtificationService.getToken()
-            }
-        })
-        .then((response) =>{
-            setImageSrc(`data:image/jpeg;base64,${response.data}`);
-        })
-        .catch(error => console.error('Erreur lors de la récupération de l\'image :', error));
-
-    }, []);
+        if(path !== ""){
+            axios.post("http://127.0.0.1:8080/api/produits/image", {
+                "path":path
+            },{
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*',
+                    'Authorization': 'Basic ' + authtificationService.getToken()
+                }
+            })
+            .then((response) =>{
+                setImageSrc(`data:image/jpeg;base64,${response.data}`);
+            })
+            .catch(error => console.error('Erreur lors de la récupération de l\'image :', error));
+        }
+    }, [path]);
     var divStyle ={  
         width: '150px'
-        
     }
     return (
         <div >
             <div style={divStyle}>
                 <br/>
-                {imageSrc && <img  width='500' height='200' src={imageSrc} alt="image" />}
+                {imageSrc && <img  width='500' height='200' src={imageSrc} alt="imageFailed" />}
                 <p><b>Vendeur :</b>{produit.utilisateurId}</p>
                 <p><b>Description :</b>{produit.description}</p>
                 <p><b>Prix :</b>{produit.prix} </p>
