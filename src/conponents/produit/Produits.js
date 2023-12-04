c839b58551a7c48c34186fd5c6284a954087efcc
Changrui <m.zhang.changrui@gmail.com>
2023-12-05 00:54:29 +0100

    filtre

--- src/conponents/produit/Produits.js
+++ src/conponents/produit/Produits.js
@@ -11,6 +11,7 @@ const Produits = ({token}) => {
     const [newProduit,setNewProdtui] = useState(false);
     const [produits, setProduits] = useState([]);  
     const [quantite,SetQuantite] = useState(1);
+    const [filtre,setFiltre] = useState("");
     // obtenir liste des produits  
     useEffect(() => {
         axios(header.getProduits())
@@ -23,16 +24,28 @@ const Produits = ({token}) => {
         flexWrap: 'wrap'
     };
     //action clique sur bouton
-        let handleClick=()=>{
+    let handleClick=()=>{
         newProduit?setNewProdtui(false):setNewProdtui(true);
     }
+    //filter
 
     if(newProduit){
         return <AddProduit/>
     }else{
     return (
         <div>
+            <div>
+                <p>filtre</p>
+                  <select name="type" id="type-select" onChange={(e)=>setFiltre(e.target.value)}>
+                     <option value="">--fitrer par type--</option>
+                     <option value="CD">CD</option>
+                     <option value="K7">K7</option>
+                     <option value="VINYLE33">VINYLE33</option>
+                     <option value="VINYLE45">VINYLE45</option>
+                  </select>
+            </div>
             <div style={divStyle}>
+        
                 {produits.map((produit, index) => 
                 {
                     let handleClick=()=>{
@@ -53,13 +66,26 @@ const Produits = ({token}) => {
                         });
                         
                     }
-                    return (
-                    <div>
-                        <ProduitCard key={produit.produitid} produit={produit} />
-                        <p>Quantité: <input onChange={(e)=>SetQuantite(e.target.value)} type='number' defaultValue={1}/></p>
-                        <button onClick={handleClick}>Ajouter au panier</button>
-                    </div>
-                    )}
+                    if(filtre !== ""){
+                        if(produit.type === filtre){
+                            return (
+                                <div>
+                                    <ProduitCard key={produit.produitid} produit={produit} />
+                                    <p>Quantité: <input onChange={(e)=>SetQuantite(e.target.value)} type='number' defaultValue={1}/></p>
+                                    <button onClick={handleClick}>Ajouter au panier</button>
+                                </div>
+                            )
+                        }
+                    }else{
+                        return (
+                            <div>
+                                <ProduitCard key={produit.produitid} produit={produit} />
+                                <p>Quantité: <input onChange={(e)=>SetQuantite(e.target.value)} type='number' defaultValue={1}/></p>
+                                <button onClick={handleClick}>Ajouter au panier</button>
+                            </div>
+                        )
+                    }
+                    }
                 )}
             </div>
             <br/>
